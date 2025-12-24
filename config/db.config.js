6e5b1044979a8ffac18d69dedb2e76ccdf8a662b
Marcel <kibormarcel90@gmail.com>
2025-12-24 19:38:02 +0300

    render db

--- config/db.config.js
+++ config/db.config.js
@@ -1,22 +1,21 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize(
-  process.env.DATABASE,
-  process.env.DB_USER, 
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'postgres',
-    logging: false,
-    define: {
-      freezeTableName: true  
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  logging: false,
+  define: {
+    freezeTableName: true
+  },
+  dialectOptions: {
+    ssl: {
+      rejectUnauthorized: false  // Crucial for Render Postgres
     }
   }
-);
+});
 
 sequelize.sync({ alter: true })
   .then(() => console.log('DB synced successfully.'))
-  .catch(err => console.error(err));
+  .catch(err => console.error('DB sync error:', err));
 
 module.exports = sequelize;
